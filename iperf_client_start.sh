01c34dda8a108c01a3855d2cf09800a13e6bfc23
joycebm <7745837+joycebm@users.noreply.github.com>
2018-02-08 12:22:30 +0200

    Update iperf_client_start.sh

--- iperf_client_start.sh
+++ iperf_client_start.sh
@@ -6,5 +6,7 @@
 file="/root/iperf-server-ip" #the file where server_configure_only.sh script stored the iperf-server ip 
 
 IPERF_SERVER_IP=$(cat "$file")
+sleep 120
 
 screen -d -m -S client iperf -c $IPERF_SERVER_IP -t 300
+dstat -T --cpu --mem --load --output report.csv 1 360
