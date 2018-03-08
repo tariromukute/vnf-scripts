e3c3d9674b3c75684bcb1c8f0fdef371b13012c4
joycebm <7745837+joycebm@users.noreply.github.com>
2018-03-08 13:02:08 +0200

    Update media-start.sh

--- media-start.sh
+++ media-start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp /root/telnet.py /home/ubuntu
+cp /root/big_buck_bunny_720p_10mb.mp4 /home/ubuntu
+
 vlc --ttl 12 -vvv --color -I telnet --telnet-password epc --rtsp-host 0.0.0.0 --rtsp-port 5554 &
 
 sleep 5
