4141c2237070dfc6cf8f202694787300c46ddae9
joycebm <7745837+joycebm@users.noreply.github.com>
2018-03-08 13:10:33 +0200

    Update media-configure.sh

--- media-configure.sh
+++ media-configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4
+sudo wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4
 
-cp big_buck_bunny_720p_10mb.mp4cp /root/
+sudo cp big_buck_bunny_720p_10mb.mp4cp /root/
 
 
 #vlc --ttl 12 -vvv --color -I telnet --telnet-password epc --rtsp-host 0.0.0.0 --rtsp-port 5554 &
