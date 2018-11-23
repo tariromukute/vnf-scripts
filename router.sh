ef7e17a8aaa46fb9bbd8e7ebe7c237b17ee59d32
joycebm <7745837+joycebm@users.noreply.github.com>
2018-11-23 11:51:29 +0200

    Update router.sh

--- router.sh
+++ router.sh
@@ -2,7 +2,6 @@ sudo apt install python3-pip -y
 sudo python3 -m  pip install tensorflow
 sudo pyhton3 -m pip install numpy
 sudo python3 -m pip install numpy
-sudo apt-get install python3-pip -y
 sudo apt install python3-numpy python3-scipy -y
 sudo python3 -m  pip install scapy
 git clone https://github.com/onerimas/Final_exe_version_1.git webtaskclassifier
