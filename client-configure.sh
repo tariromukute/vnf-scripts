5c90fc133678743cc1021f623746f15906b67a9e
joycebm <7745837+joycebm@users.noreply.github.com>
2018-12-11 23:08:55 +0200

    Update client-configure.sh

--- client-configure.sh
+++ client-configure.sh
@@ -1,4 +1,12 @@
-echo $router_dummy
-echo "$router_dummy" > /root/router_ip
-ip route replace default via $router_dummy dev eth0
+if [ $router_shared_int_network_floatingIp ]
+then
+    export routeip=$router_shared_int_network_floatingIp
+else
+    export routeip=$router_dummy_floatingIp
+fi
+
+
+echo $routeip
+echo "$routeip" > /root/router_ip
+ip route replace default via $routeip dev eth0
 ip route
